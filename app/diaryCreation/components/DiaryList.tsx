a88a3eff264d730af0583e7991cda80ff4245ea6
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-22 01:14:54 +0900

    日記一覧画面

--- app/diaryCreation/components/DiaryList.tsx
+++ app/diaryCreation/components/DiaryList.tsx
@@ -1,26 +1,49 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Image } from 'expo-image'
+// import { addLineBreaks } from '../action/addLineBreaks'
 
 export default function DiaryList() {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const userImage = require('../../../assets/images/user.png');
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const noImage = require('../../../assets/images/no_image.png');
+
 
   return (
     <View style={styles.diaryList}>
       <View style={styles.diaryDateContainer}>
-        <Text style={styles.diaryDate}>6月18日(水)</Text>
+        <Text style={styles.diaryDay}>6月18日(水)</Text>
       </View>
-      <View>
+
+
+
+
+
+
+      <View style={styles.diaryContentContainer}>
         {/* 日記作成者のアイコン画像 */}
-        <View>
+        <View style={styles.diaryCreateUserIconContainer}>
           <Image
             source={userImage}
             style={styles.diaryCreateUserIcon}
           />
         </View>
-        <Text>今日は上司の佐藤さんに褒められて</Text>
-        <Text>嬉しい1日だった。何を褒められ．．．</Text>
+        {/* 日記内容 */}
+        <View style={styles.diaryContent}>
+          <Text>21:22</Text>
+          <Text numberOfLines={3} ellipsizeMode="tail">
+            {/* {formattedText} */}
+            今日は上司の佐藤さんに褒められて嬉しい1日だった。何を褒められ．．．
+          </Text>
+        </View>
+        {/* 日記投稿画像 */}
+        <View style={styles.diaryImageContainer}>
+          <Image
+            source={noImage}
+            style={styles.diaryImage}
+          />
+        </View>
       </View>
     </View>
   )
@@ -35,13 +58,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0F0F0',
     paddingHorizontal: 24,
   },
-  diaryDate: {
+  diaryDay: {
     fontSize: 16,
     lineHeight: 30,
     fontWeight: 'bold',
   },
+  diaryContentContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+  },
+  diaryCreateUserIconContainer: {
+    marginRight: 16,
+  },
+  diaryContent: {
+    //
+  },
   diaryCreateUserIcon: {
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
+  },
+  diaryImageContainer: {
+    marginLeft: 'auto',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  diaryImage: {
+    width: 80,
+    height: 80,
   },
 })
