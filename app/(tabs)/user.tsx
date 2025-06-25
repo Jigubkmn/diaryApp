37729ea5dfb70f8c3f3d0e655bf023a18f7c26af
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-26 02:10:16 +0900

    ユーザーアイコン画像を表示

--- app/(tabs)/user.tsx
+++ app/(tabs)/user.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
 import Header from '../user/components/Header'
 import DiaryShareInfo from '../user/components/DiaryShareInfo'
+import { Image } from 'expo-image'
+import EditIcon from '../../components/Icon/EditIcon';
+
 
 export default function user() {
+
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const userImage = require('../../assets/images/user.png')
   return (
     <SafeAreaView style={styles.container}>
       <Header />
       <View style={styles.bodyContainer}>
+        <View style={styles.userInfoContainer}>
+          <View style={styles.userImageContainer}>
+            {/* ユーザー画像 */}
+            <Image source={userImage} style={styles.userImage} />
+            <TouchableOpacity style={styles.editIconOverlay} onPress={() => {}}>
+              <EditIcon size={24} color="#FFA500" />
+            </TouchableOpacity>
+          </View>
+          {/* <View style={styles.userIDContainer}>
+            <Text style={styles.userIDTitle}>ユーザーID</Text>
+            <Text style={styles.userIDText}>AAAABBBBCCCCDDDD</Text>
+          </View> */}
+        </View>
         <View style={styles.diaryShareContainer}>
           <Text style={styles.diaryShareTitle}>日記共通相手</Text>
           <View style={styles.diaryShareInfoContainer}>
@@ -30,6 +49,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F0F0F0',
     paddingHorizontal: 48,
+  },
+  userInfoContainer: {
+    flex: 1,
+    padding: 16,
+    marginTop: 16,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  userImageContainer: {
+    width: 100,
+    height: 100,
+    position: 'relative',
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+  },
+  editIconOverlay: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#FFA500',
+    backgroundColor: '#ffffff',
+    padding: 3,
   },
   diaryShareContainer: {
     flex: 1,
