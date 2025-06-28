202ebc0b740cff2901de94dd3e8c3c1375cffc46
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-28 09:44:52 +0900

    ファイル名をuserからmyPageに変更

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -43,7 +43,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="user"
+        name="myPage"
         options={{
           title: "マイページ",
           headerShown: false,
