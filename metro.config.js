ea11f9533aa9c390d706f152fc84765d8642e305
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-29 10:33:54 +0900

    設定を追加

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line
 const { getDefaultConfig } = require('@expo/metro-config')
 
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname)
 config.resolver.sourceExts.push('cjs')
 config.resolver.unstable_enablePackageExports = false
 
+// eslint-disable-next-line no-undef
 module.exports = config
