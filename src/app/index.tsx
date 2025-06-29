bc9e8e26eb10be446b4a9c5720d593bb486ec6c0
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-29 17:17:18 +0900

    ユーザーログイン状態を監視

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Redirect, router } from 'expo-router';
+import { auth } from '../config';
+import { onAuthStateChanged } from 'firebase/auth';
 
 export default function Index() {
+  useEffect(() => {
+    // ログイン状態を監視
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.replace("/(tabs)")
+      }
+    })
+  }, [])
   return <Redirect href="/auth/login" />;
 }
