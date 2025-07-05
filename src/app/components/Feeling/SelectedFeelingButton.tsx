0814647bb71cf007055e0e6b0eddefa7b7f81b0c
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-05 23:56:45 +0900

    Feelignのコンポーネントを用意

---
+++ src/app/components/Feeling/SelectedFeelingButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Text, StyleSheet, TouchableOpacity, Image, ImageSourcePropType } from 'react-native';
+
+type Props = {
+  selectedFeeling: string | null;
+  handleFeelingSelect: (feelingName: string) => void;
+  name: string;
+  image: ImageSourcePropType | undefined;
+}
+
+export default function SelectedFeelingButton({ selectedFeeling, handleFeelingSelect, name, image }: Props) {
+  return (
+    <TouchableOpacity
+      key={name}
+      style={styles.feelingItem}
+      onPress={() => handleFeelingSelect(name)}
+    >
+      <Image
+        source={image}
+        style={[
+          styles.feelingImage,{ opacity: selectedFeeling === name ? 1 : 0.4 }]}
+      />
+      <Text style={[styles.feelingText, { opacity: selectedFeeling === name ? 1 : 0.4 }]}>{name}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  feelingItem: {
+    alignItems: 'center',
+  },
+  feelingImage: {
+    width: 40,
+    height: 40,
+  },
+  feelingText: {
+    fontSize: 14,
+    lineHeight: 24,
+  },
+});
