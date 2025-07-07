2fdb9c3ab8fe51a12daf157c4f33337eda83a6be
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-07 22:59:15 +0900

    対象日記情報取得関数を共通化対応

---
+++ src/app/actions/fetchSelectedDiary.ts
@@ -0,0 +1,35 @@
+import { db } from '../../config';
+import { doc, getDoc } from 'firebase/firestore';
+import dayjs from 'dayjs';
+import { DiaryType } from '../../../type/diary';
+
+type Props = {
+  userId?: string;
+  diaryId?: string;
+  setSelectedDiaryInfo: (diary: DiaryType) => void;
+}
+
+
+export default async function fetchSelectedDiary({ userId, diaryId, setSelectedDiaryInfo }: Props) {
+  if (userId === null || diaryId === null) return;
+    try {
+      const diaryRef = doc(db, `users/${userId}/diary/${diaryId}`);
+      const diarySnap = await getDoc(diaryRef);
+      if (diarySnap.exists()) {
+        const data = diarySnap.data();
+        const diary: DiaryType = {
+          id: diarySnap.id,
+          diaryText: data.diaryText,
+          diaryDate: dayjs(data.diaryDate.toDate()),
+          feeling: data.feeling || null,
+          updatedAt: data.updatedAt.toDate(),
+          selectedImage: data.selectedImage
+        };
+        setSelectedDiaryInfo(diary);
+      } else {
+        console.log('対象データがありません。');
+      }
+    } catch (error) {
+      console.error('対象データの取得に失敗しました。', error);
+  }
+}
