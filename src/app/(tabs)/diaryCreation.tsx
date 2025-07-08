c72d3c722837e457d5e2a355e84d43f73d2cad0e
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-08 15:41:32 +0900

    日記作成成功時に画像をリセット

--- src/app/(tabs)/diaryCreation.tsx
+++ src/app/(tabs)/diaryCreation.tsx
@@ -35,6 +35,7 @@ export default function DiaryCreation() {
             selectedFeeling={selectedFeeling}
             setDiaryText={setDiaryText}
             setSelectedFeeling={setSelectedFeeling}
+            setSelectedImage={setSelectedImage}
             isShowBackButton={isShowBackButton === 'true'}
             selectedImage={selectedImage}
           />
