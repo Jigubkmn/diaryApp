65add132ada4b1880985de55fbc6cd6b3c9ceb36
Masashi Shiomi <bkmn.31519@gmail.com>
2025-07-13 01:24:23 +0900

    ユーザー画像を用意

--- src/app/constants/userImage.ts
+++ src/app/constants/userImage.ts
@@ -3,7 +3,13 @@ export const noUserImage = require('../../../assets/images/no_user_image.png');
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 export const noImage = require('../../../assets/images/no_image.png');
 
+// 画像パスの文字列
+export const NO_USER_IMAGE_PATH = '../../../assets/images/no_user_image.png';
+export const NO_IMAGE_PATH = '../../../assets/images/no_image.png';
+
 export default {
   noUserImage,
   noImage,
+  NO_USER_IMAGE_PATH,
+  NO_IMAGE_PATH,
 };
